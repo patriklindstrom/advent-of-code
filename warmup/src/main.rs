a4f3b3049ba39c9b5b76622af9824fb2dc04f380
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2023-11-30 23:22:54 +0100

    baseline add test to read simple csv file

--- warmup/src/main.rs
+++ warmup/src/main.rs
@@ -1,3 +1,25 @@
+use csv::ReaderBuilder;
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
 fn main() {
-    println!("Hello, world!");
+    #[derive(Debug, Deserialize)]
+    struct Record {
+        key: i32,
+        value: String,
+    }
+    fn read_csv(file_path: &str) -> Result<(), Box<dyn Error>> {
+        let mut rdr = ReaderBuilder::new().has_headers(false).from_reader(File::open(file_path)?);
+        for result in rdr.deserialize() {
+            let record: Record = result?;
+            println!("key: {}, value: {}", record.key, record.value);
+        }
+        Ok(())
+    }
+    fn main() {
+        if let Err(err) = read_csv("testdata/data.csv") {
+            eprintln!("Error reading CSV file: {}", err);
+        }
+    }
+    main();
 }
