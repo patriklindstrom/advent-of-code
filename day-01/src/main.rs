23de844e9f241026438a91c36c4c98aadbd8e8a3
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2023-12-01 12:50:45 +0100

    My answer is 55330 which is not correct for second star

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-
+use std::collections::HashMap;
 fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
     where
         P: AsRef<Path>,
@@ -10,9 +10,40 @@ fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
     let buffer = io::BufReader::new(file);
     buffer.lines().collect()
 }
+
 fn find_digits(s: &str) -> Vec<char> {
-    s.chars().filter(|c| c.is_digit(10)).collect()
+    let map = number_word_map();
+    let mut result = Vec::new();
+    let mut current_index = 0;
+
+    while current_index < s.len() {
+        let mut longest_match = None;
+        for end in (current_index + 1)..=s.len() {
+            let substring = &s[current_index..end];
+            if map.contains_key(substring) {
+                longest_match = Some(substring);
+            }
+        }
+
+        if let Some(matching_word) = longest_match {
+            if let Some(&digit) = map.get(matching_word) {
+                result.push(digit);
+            }
+            current_index += matching_word.len(); // Skip past the matched word
+        } else {
+            current_index += 1; // No match found, move one character forward
+        }
+    }
+
+    result
 }
+
+// Rest of your code, including the `number_word_map` function, remains the same.
+
+
+
+
+
 fn concatenate_first_last(digits: Vec<char>) -> Option<i32> {
     match digits.first().zip(digits.last()) {
         Some((&first, &last)) => {
@@ -22,8 +53,24 @@ fn concatenate_first_last(digits: Vec<char>) -> Option<i32> {
         None => None,
     }
 }
+fn number_word_map() -> HashMap<&'static str, char> {
+    let mapping = [
+        ("0", '0'), ("zero", '0'),
+        ("1", '1'), ("one", '1'),
+        ("2", '2'), ("two", '2'),
+        ("3", '3'), ("three", '3'),
+        ("4", '4'), ("four", '4'),
+        ("5", '5'), ("five", '5'),
+        ("6", '6'), ("six", '6'),
+        ("7", '7'), ("seven", '7'),
+        ("8", '8'), ("eight", '8'),
+        ("9", '9'), ("nine", '9'),
+    ];
+    mapping.iter().cloned().collect()
+}
 fn main() {
-    let file_path = "puzzledata/calibrationdata.txt";
+     let file_path = "puzzledata/calibrationdata.txt";
+    // let file_path = "puzzledata/testdata.txt";
     match read_lines(file_path) {
         Ok(lines) => {
             let mut sum = 0;
@@ -32,11 +79,11 @@ fn main() {
             println!("Number of no_of_records in file: {}", no_of_records);
 
             for cal_val in lines {
-                // println!("{}", cal_val);
+                 println!("{}", cal_val);
                 let digits = find_digits(&*cal_val);
-                // println!("Digits found: {:?}", digits);
+                 println!("Digits found: {:?}", digits);
                 if let Some(number) = concatenate_first_last(digits) {
-                   // println!("String: '{}', Number: {}", cal_val, number);
+                    println!("String: '{}', Number: {}", cal_val, number);
                     sum += number;
                 } else {
                     println!("***  String: '{}', No digits found", cal_val);
