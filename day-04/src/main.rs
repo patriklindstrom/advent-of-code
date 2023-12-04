9c8aea39f0b7df957ae5f44dc7d3bdb7a7c6e1b8
Patrik Lindstrom <patrik.lindstrom@lcube.se>
2023-12-04 09:54:04 +0100

    Day-04 correct answere silver start

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() -> io::Result<()> {
-    let path = Path::new("testdata/cards.txt");
+    let path = Path::new("puzzledata/input.txt");
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
     let mut total_points = 0;
