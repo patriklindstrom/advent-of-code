bd2260bf271b9802a4fe8070a11d505048405cf2
Patrik Lindström <patrik.lindstrom@lcube.se>
2023-12-04 09:56:36 +0100

    Feature/day 04 Silver Star (#6)
    * Start day-04
    
    * Day-04 testdata works
    
    * Day-04 correct answere silver star

---
+++ day-04/src/main.rs
@@ -0,0 +1,47 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() -> io::Result<()> {
+    let path = Path::new("puzzledata/input.txt");
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+    let mut total_points = 0;
+    for line in reader.lines() {
+        let line = line?;
+        let parts: Vec<&str> = line.split(" | ").collect();
+        if parts.len() != 2 {
+            continue; // Skip lines that don't match the format
+        }
+        // Split the line into winning numbers and my numbers
+        let winning_numbers: Vec<i32> = parts[0].split_whitespace().filter_map(|s| s.parse().ok()).collect();
+        let my_numbers: Vec<i32> = parts[1].split_whitespace().filter_map(|s| s.parse().ok()).collect();
+        // Calculate and print the points for this card
+        let points = calculate_points(&winning_numbers, &my_numbers);
+        println!("Card: {}", parts[0]);
+        println!("Winning numbers found: {:?}", winning_numbers.iter().filter(|&&num| my_numbers.contains(&num)).collect::<Vec<_>>());
+        println!("Points for this card: {}\n", points);
+        total_points += points;
+    }
+    println!("The Elf's pile of scratchcards is worth in total : {} points", total_points);
+    Ok(())
+}
+
+fn calculate_points(winning_numbers: &[i32], my_numbers: &[i32]) -> i32 {
+    let mut points = 0;
+    let mut first_match = true;
+
+    for &num in winning_numbers {
+        if my_numbers.contains(&num) {
+            if first_match {
+                points += 1;
+                first_match = false;
+            } else {
+                points *= 2;
+            }
+        }
+    }
+
+    points
+}
+
